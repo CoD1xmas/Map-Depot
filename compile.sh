764ec41f11620e2dd7e6f17fc295d779ce93866d
Cato <121131666+cato-a@users.noreply.github.com>
2023-12-11 22:44:39 +0100

    Small adjustments

--- compile.sh
+++ compile.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
 
 FSUFFIX='zZz_XMAS_EVENT_v23_pak'
-FPOSTFIX='.pk3'
-ZIPSPLIT=157286400
+ZIPSPLIT=134217728 # 128 MiB
 ZIPTOTAL=0
 PAK=0
 
 while IFS='' read -r L; do
-    if [[ "${L:0:1}" == "#" || -z "$L" ]]; then
+    if [[ "${L:0:1}" == '#' || -z "$L" ]]; then
         continue
     fi
 
     ZIPTOTAL=$(( ZIPTOTAL + "$(du -s "maps/$L" | cut -f1)" ))
 
-    7z a -tzip -x'@ignorelist' "$FSUFFIX$PAK$FPOSTFIX" "./maps/$L/"* # /dev/null 2>&1
+    7z a -tzip -x'@ignorelist' "$FSUFFIX$PAK.pk3" "./maps/$L/"* # /dev/null 2>&1
 
     if [[ "$ZIPTOTAL" -gt "$ZIPSPLIT" ]]; then
         PAK=$(( PAK + 1 ))
