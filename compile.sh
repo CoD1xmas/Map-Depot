a0df39414c31ea76523ee1cd3e7f62d6936152b7
thecheeseman <1952103+thecheeseman@users.noreply.github.com>
2023-12-13 11:53:49 -0700

    feat: update compile script to search for different 7zip exes

--- compile.sh
+++ compile.sh
@@ -5,6 +5,18 @@ ZIPSPLIT=134217728 # 128 MiB
 ZIPTOTAL=0
 PAK=0
 
+ZIPCOMMAND=`command -v 7z`
+if [[ -z "$ZIPCOMMAND" ]]; then
+    ZIPCOMMAND=`command -v 7za`
+fi
+if [[ -z "$ZIPCOMMAND" ]]; then
+    ZIPCOMMAND=`command -v 7zz`
+fi
+if [[ -z "$ZIPCOMMAND" ]]; then
+    echo "7zip not available"
+    exit
+fi
+
 while IFS='' read -r L; do
     if [[ "${L:0:1}" == '#' || -z "$L" ]]; then
         continue
@@ -16,7 +28,7 @@ while IFS='' read -r L; do
 
     ZIPTOTAL=$(( ZIPTOTAL + "$(du -s "maps/$L" | cut -f1)" ))
 
-    7z a -tzip "$FSUFFIX$PAK.pk3" "./maps/$L/"* # /dev/null 2>&1
+    $ZIPCOMMAND a -tzip "$FSUFFIX$PAK.pk3" "./maps/$L/"* # /dev/null 2>&1
 
     if [[ "$ZIPTOTAL" -gt "$ZIPSPLIT" ]]; then
         PAK=$(( PAK + 1 ))
